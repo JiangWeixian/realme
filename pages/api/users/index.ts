fb2936157981b6595ad22b415996580791177cd9
jiangWeixian <jiangweixian1994@gmail.com>
2021-04-23 02:16:51 +0800

    ⏪ reset to init viewbox size version

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export const createYearOfProgress = ({ color = 'black', scale = 1 }: { color: string, scale: string | number }) => {
   return `
-<svg width="22px" height="26px" viewBox="0 -4 22 26" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg width="22px" height="22px" viewBox="0 0 22 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <foreignObject width="100%" height="100%">
     <div xmlns="http://www.w3.org/1999/xhtml">
       <style>
