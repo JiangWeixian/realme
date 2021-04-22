0ef8b2f11892e4d209fa8652cb544edd7665efed
jiangWeixian <jiangweixian1994@gmail.com>
2021-04-23 01:21:52 +0800

    🚧 cssgg mvp

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,16 +1,55 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { sampleUserData } from '../../../utils/sample-data'
 
-const handler = (_req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    if (!Array.isArray(sampleUserData)) {
-      throw new Error('Cannot find user data')
-    }
-
-    res.status(200).json(sampleUserData)
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message })
-  }
+export const createYearOfProgress = ({ color = 'black' }: { color: string }) => {
+  return `
+<svg width="800px" height="100px" viewBox="0 0 800 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <foreignObject width="100%" height="100%">
+    <div xmlns="http://www.w3.org/1999/xhtml">
+      <style>
+        .gg-add-r {
+          box-sizing: border-box;
+          position: relative;
+          display: block;
+          width: 22px;
+          height: 22px;
+          border: 2px solid;
+          transform: scale(var(--ggs,1));
+          border-radius: 4px;
+          color: ${color}
+      }
+      .gg-add-r::after,
+      .gg-add-r::before {
+          content: "";
+          display: block;
+          box-sizing: border-box;
+          position: absolute;
+          width: 10px;
+          height: 2px;
+          background: currentColor;
+          border-radius: 5px;
+          top: 8px;
+          left: 4px
+      }
+      .gg-add-r::after {
+          width: 2px;
+          height: 10px;
+          top: 4px;
+          left: 8px
+      }
+      </style>
+      <i class="gg-add-r"></i>
+    </div>
+  </foreignObject>
+</svg>
+  `
 }
 
-export default handler
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const yearOfProgress = createYearOfProgress({ color: req.query.color as string })
+    res.setHeader('Content-Type', 'image/svg+xml')
+    res.status(200).end(yearOfProgress)
+  } catch (err) {
+    res.status(404).json({ statusCode: 404, message: err.message })
+  }
+}
