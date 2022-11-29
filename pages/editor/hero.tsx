cc5b311c987600ef5cc61baf54c7afadda85a4a3
JiangWeixian <jiangweixian1994@gmail.com>
2022-11-29 23:56:01 +0800

    :lipstick:  hero editor bg

--- pages/editor/hero.tsx
+++ pages/editor/hero.tsx
@@ -10,7 +10,15 @@ const Page = () => {
     subtitle: 'world',
     desc: 'hello wrold',
   })
-  return <div className="flex justify-center items-center w-screen h-screen">{banner}</div>
+  return (
+    <div className="antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 w-screen h-screen">
+      <div className="flex justify-center items-center not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25 w-screen h-screen">
+        <div className="absolute inset-0 bg-grid [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]" />
+        <div className="shadow-lg rounded-xl hover:shadow-2xl">{banner}</div>
+        <div className="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5" />
+      </div>
+    </div>
+  )
 }
 
 export default Page
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    bgGrid: () => ({
+      100: {
+        backgroundImage:
+          "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='#f1f5f9'><path d='M0 .5H31.5V32'/></svg>",
+      },
+      700: {
+        backgroundImage:
+          "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke='rgb(51 65 85 / 0.25)'><path d='M0 .5H31.5V32'/></svg>",
+      },
+    }),
   },
 }
