06fa7cc6407a5ca85a8db8a73520a64032e405f4
JW <jiangweixian1994@gmail.com>
2022-12-06 10:10:33 +0800

    ✨ r3f (#12)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
+        ) as any,
       }
     } finally {
       sheet.seal()
