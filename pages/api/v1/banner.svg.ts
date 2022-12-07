e0ded32edab31c51f0235ac41efac8e73f15851a
JW <jiangweixian1994@gmail.com>
2022-12-07 23:26:14 +0800

    🐛 banner.svg endpoints (#13)

---
+++ pages/api/v1/banner.svg.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { renderToString } from 'react-dom/server'
+
+import { Banner } from '@/components/banner'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { colorA, colorB, title, subtitle, textColor, desc } = req.query as any
+    const banner = renderToString(Banner({ colorA, colorB, title, subtitle, textColor, desc }))
+    res.setHeader('Content-Type', 'image/svg+xml')
+    res.status(200).end(banner)
+  } catch (err: any) {
+    res.status(404).json({ statusCode: 404, message: err.message })
+  }
+}
