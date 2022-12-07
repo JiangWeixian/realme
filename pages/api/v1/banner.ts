ef518659dd4c4bd9e9526d8ede656a0e71c6a98f
JiangWeixian <jiangweixian1994@gmail.com>
2022-12-07 23:23:30 +0800

    :bug:  banner.svg endpoints

--- pages/api/v1/banner.ts
+++ pages/api/v1/banner.ts
@@ -1,15 +1,6 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { renderToString } from 'react-dom/server'
+/**
+ * @fileoverview compatiable reason, /api/v1/banner same as /api/v1/banner.svg
+ */
+import BannerSvgHandler from './banner.svg'
 
-import { Banner } from '@/components/banner'
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { colorA, colorB, title, subtitle, textColor, desc } = req.query as any
-    const banner = renderToString(Banner({ colorA, colorB, title, subtitle, textColor, desc }))
-    res.setHeader('Content-Type', 'image/svg+xml')
-    res.status(200).end(banner)
-  } catch (err: any) {
-    res.status(404).json({ statusCode: 404, message: err.message })
-  }
-}
+export default BannerSvgHandler
