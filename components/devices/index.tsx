e64d3498ac9cc9b228b8a0058ff30d4f86abb8a0
JiangWeixian <jiangweixian1994@gmail.com>
2023-01-02 15:34:51 +0800

    :sparkles:  lists

--- components/devices/index.tsx
+++ components/devices/index.tsx
@@ -2,32 +2,34 @@ import React from 'react'
 import { StyledWebGradients } from 'styled-webgradients/utils'
 
 import { $styles } from './styles'
-import { Svg } from 'components/svg'
+import { Svg, SvgProps } from 'components/svg'
 import { cssText } from 'lib/format'
 
 const sw = new StyledWebGradients()
 
-type DevicesProps = {
+export type DevicesProps = {
   title?: string
   desc?: string
   rawStyles?: string
   rawContent?: string
   imgSrc?: string
-}
+  webgradientsName?: string
+} & Pick<SvgProps, 'width' | 'height' | 'responsive'>
 
 export const DeviceIphoneX = (props: DevicesProps) => {
   return (
-    <div className="relative h-full w-full">
+    <div className="relative h-full w-full flex justify-center items-center">
       <div
         style={{ position: 'absolute', width: '100%', height: '100%' }}
         className="absolute webgradients-bg h-full w-full z-0"
       />
-      <div className="flex justify-between items-center w-full h-full p-24">
+      <div className="flex justify-between items-center h-full w-10/12">
         <div className="text-left relative">
-          <h1 className="text-white text-7xl font-bold">{props.title}</h1>
-          <p className="text-gray-200 text-4xl">{props.desc}</p>
+          <h1 className="text-white text-4xl font-bold">{props.title}</h1>
+          <p className="text-gray-200 text-base">{props.desc}</p>
         </div>
-        <div className="drop-shadow-md device device-iphone-x">
+        {/* trick: scale to make sure display pefert in [800, 400] scale */}
+        <div className="drop-shadow-md device device-iphone-x transform-gpu scale-40">
           <div className="device-frame">
             <img className="device-screen" src={props.imgSrc} />
           </div>
@@ -46,7 +48,7 @@ export const DeviceIPadPro = (props: DevicesProps) => {
   return (
     <div className="relative h-full w-full flex justify-center items-center">
       <div className="absolute webgradients-bg h-full w-full z-0" />
-      <div className="drop-shadow-md scale-150 origin-center transform -rotate-90 device device-ipad-pro">
+      <div className="drop-shadow-md scale-65 origin-center transform -rotate-90 device device-ipad-pro">
         <div className="device-frame">
           <img className="device-screen" src={props.imgSrc} />
         </div>
@@ -61,11 +63,17 @@ export const DeviceIPadPro = (props: DevicesProps) => {
   )
 }
 
+/**
+ * @description Display snapshot img inside devices, currently only support [800, 400]
+ */
 export const Devices = (props: DevicesProps) => {
-  const gradientCss = sw.unstable_buildRadialGradient('HappyFisher')
+  const gradientCss = sw.unstable_buildRadialGradient(
+    (props.webgradientsName ?? 'HappyFisher') as any,
+  )
   return (
     <Svg
-      responsive={true}
+      width={props.width}
+      height={props.height}
       style={
         <style
           dangerouslySetInnerHTML={{
---
+++ pages/list.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { API_URL } from 'lib/constants'
+import Docs from 'assets/docs.svg'
+import Editor from 'assets/editor.svg'
+
+const wigets = [
+  {
+    name: 'banner',
+    editor: '/editor/banner',
+    url: '/banner.svg?colorA=831843&colorB=be185d&textColor=f472b6&title=World!&subtitle=Hello',
+  },
+  {
+    name: 'webgradients',
+    editor: '/editor/webgradients',
+    url: '/webgradients.svg?title=hello+world&subtitle=indiehackers&desc=happy+hacking&radialgradiets=true&webgradientsName=HappyFisher',
+  },
+  {
+    name: 'typical',
+    editor: '/editor/typical',
+    url: '/typical.svg?steps=hello&steps=1000&steps=hello%20world&steps=500&steps=hello%20realme',
+  },
+  {
+    name: 'mdx',
+    docs: 'https://github.com/JiangWeixian/realme#mdx',
+    url: '/mdx.svg?remoteContent=https://ungh.cc/repos/JiangWeixian/realme/files/master/docs/mdx.mdx&width=660&height=1100',
+  },
+  {
+    name: 'devices-iphonex',
+    docs: 'https://github.com/JiangWeixian/realme#devices',
+    url: '/devices.svg?webgradientsName=BlueVelvet&width=800&height=400&type=iphonex&imgSrc=https://images.unsplash.com/photo-1527842891421-42eec6e703ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+  },
+  {
+    name: 'devices-ipadpro',
+    docs: 'https://github.com/JiangWeixian/realme#devices',
+    url: '/devices.svg?webgradientsName=WinterNeva&width=800&height=400&type=ipadpro&imgSrc=https://images.unsplash.com/photo-1527842891421-42eec6e703ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+  },
+]
+
+const List = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-slate-50 dark:bg-slate-900 overflow-x-hidden overflow-y-auto py-4 pt-12">
+      <div className="w-10/12 mb-8">
+        <h1 className="text-4xl text-white font-bold font-mono text-left">Available Wigets</h1>
+        <p className="text-base text-slate-50/70 text-left">
+          Click icon to see all endpoint params
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-8 py-4 w-10/12 min-h-screen">
+        {wigets.map((wiget) => {
+          return (
+            <div className="rounded-lg" key={wiget.name}>
+              <div className="rounded-lg shadow-lg ring ring-transparent hover:ring-blue-400/70 ring-offset-4 dark:ring-offset-slate-900 h-fit">
+                <img src={`${API_URL}${wiget.url}`} alt={wiget.name} />
+              </div>
+              <div className="flex items-center mt-4 cursor-pointer">
+                {wiget.docs && (
+                  <a href={wiget.docs} rel="noopener noreferrer" target="_blank">
+                    <Docs className="text-slate-400 w-4 h-4" />
+                  </a>
+                )}
+                {wiget.editor && (
+                  <Link href={wiget.editor}>
+                    <Editor className="text-slate-400 w-4 h-4" />
+                  </Link>
+                )}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default List
